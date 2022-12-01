6b24b8099ce10125af4e687ec8c62688239ae704
Ben Sterling <25365573+ben-styling@users.noreply.github.com>
2022-12-01 20:35:34 +0000

    rust - day 1 - part 2

--- rust/src/day01.rs
+++ rust/src/day01.rs
@@ -1,5 +1,5 @@
 use std::fs;
-pub fn part1(input: &str) -> u32 {
+fn part1(input: &str) -> u32 {
     let result = input
         .split("\n\n")
         .map(|snacks| {
@@ -10,14 +10,35 @@ pub fn part1(input: &str) -> u32 {
         })
         .max()
         .unwrap();
-
-    print!("{:?}", result);
     return result;
+}
+
+fn part2(input: &str) -> u32 {
+    let mut result = input
+        .split("\n\n")
+        .map(|snacks| {
+            snacks
+                .split("\n")
+                .filter_map(|snack| snack.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
+
+    result.sort_by(|a,b| b.cmp(a));
+
+    return result.iter().take(3).sum();
 }
 
 pub fn solve_part_1() {
     let input = fs::read_to_string("src/day01.input.txt").unwrap();
-    part1(&input);
+    let result = part1(&input);
+    println!("{:?}", result);
+}
+
+pub fn solve_part_2() {
+    let input = fs::read_to_string("src/day01.input.txt").unwrap();
+    let result = part2(&input);
+    println!("{:?}", result);
 }
 
 #[cfg(test)]
@@ -25,17 +46,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_simple() {
+    fn part_1_test_simple() {
         let input: &str = "1\n\n2";
         let result = part1(input);
         let expected: u32 = 2;
         assert_eq!(result, expected);
     }
     #[test]
-    fn test_complex() {
+    fn part_1_test_complex() {
         let input: &str = "1\n\n2\n\n1000\n\n5\n5\n5\n5\n5";
         let result = part1(input);
         let expected: u32 = 1000;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn part_2_test_simple() {
+        let input: &str = "2\n\n2\n\n2\n\n1";
+        let result = part2(input);
+        let expected: u32 = 6;
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn part_2_test_complex() {
+        let input: &str = "2\n\n2\n\n2000\n\n5\n5\n5\n5\n5\n6\n\n2\n\n2";
+        let result = part2(input);
+        let expected: u32 = 2033;
         assert_eq!(result, expected);
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,5 +2,7 @@ mod day01;
 
 fn main() {
     println!("day 1 answer!");
-    day01::solve_part_1()
+    day01::solve_part_1();
+    println!("day 1 answer part 2!");
+    day01::solve_part_2();
 }
