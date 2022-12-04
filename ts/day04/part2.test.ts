0f2d0f32be591d52469ed1c86c704ce3f43447ed
Ben Sterling <25365573+ben-styling@users.noreply.github.com>
2022-12-04 18:10:13 +0000

    day 4 - part 2

--- ts/day04/part2.test.ts
+++ ts/day04/part2.test.ts
@@ -2,7 +2,12 @@ import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { solve } from "./part2.ts";
 
 Deno.test("solve", () => {
-  const input = ``;
+  const input = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
   const result = solve(input);
-  assertEquals(result, 0);
+  assertEquals(result, 4);
 });
--- ts/day04/part2.ts
+++ ts/day04/part2.ts
@@ -1,5 +1,13 @@
 export const solve = (input: string) => {
-  return input.split("\n");
+  return input.split("\n").reduce((acc, line) => {
+    const [left, right] = line.split(",");
+    const [leftMin, leftMax] = left.split("-").map((v) => Number(v));
+    const [rightMin, rightMax] = right.split("-").map((v) => Number(v));
+    if (rightMax >= leftMin && leftMax >= rightMin) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 };
 
 console.log(solve(await Deno.readTextFile("day04/input.txt")));
