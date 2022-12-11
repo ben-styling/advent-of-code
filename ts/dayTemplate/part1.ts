6f38db3e0156db97230cd65b571139f2f7b2fecf
Ben Sterling <25365573+ben-styling@users.noreply.github.com>
2022-12-11 16:49:19 +0000

    add day template

---
+++ ts/dayTemplate/part1.ts
@@ -0,0 +1,5 @@
+export const solve = (input: string) => {
+  return input;
+};
+
+// console.log(solve(await Deno.readTextFile("day09/input.txt")));
---
+++ ts/dayTemplate/part2.test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { solve } from "./part2.ts";
+
+const input = ``;
+
+Deno.test("solve", () => {
+  assertEquals(solve(input), input);
+});
