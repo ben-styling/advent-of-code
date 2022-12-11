1ba2b17db5d4ff28f5604636031a3c29ab67f18f
Ben Sterling <25365573+ben-styling@users.noreply.github.com>
2022-12-11 16:16:57 +0000

    day 9 - part 2

--- ts/day09/part2.test.ts
+++ ts/day09/part2.test.ts
@@ -1,7 +1,73 @@
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { solve } from "./part2.ts";
-const input = ``;
+
+import { Coords, moveHead, moveTail, solve } from "./part2.ts";
+
+const input = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`;
+
+Deno.test("moves", () => {
+  let head: [number, number] = [0, 0];
+  let tail: [number, number] = [0, 0];
+  head = moveHead(head, "U");
+  assertEquals(head, [1, 0]);
+  tail = moveTail(head, tail);
+  assertEquals(tail, [0, 0]);
+
+  head = moveHead(head, "U");
+  assertEquals(head, [2, 0]);
+  tail = moveTail(head, tail);
+  assertEquals(tail, [1, 0]);
+
+  head = moveHead(head, "U");
+  assertEquals(head, [3, 0]);
+  tail = moveTail(head, tail);
+  assertEquals(tail, [2, 0]);
+
+  head = moveHead(head, "R");
+  assertEquals(head, [3, 1]);
+  tail = moveTail(head, tail);
+  assertEquals(tail, [2, 0]);
+
+  head = moveHead(head, "R");
+  assertEquals(head, [3, 2]);
+  tail = moveTail(head, tail);
+  assertEquals(tail, [3, 1]);
+
+  head = moveHead(head, "R");
+  assertEquals(head, [3, 3]);
+  tail = moveTail(head, tail);
+  assertEquals(tail, [3, 2]);
+});
+
+Deno.test("jump", () => {
+  let tail: Coords = [0, 1];
+  tail = moveTail([2, 0], tail);
+  assertEquals(tail, [1, 0]);
+});
 
 Deno.test("solve", () => {
-  assertEquals(solve(input), input);
+  assertEquals(solve(`R 10`), 2);
+});
+Deno.test("solve", () => {
+  assertEquals(solve(`R 11`), 3);
+});
+Deno.test("solve", () => {
+  assertEquals(solve(`R 11\nU 1`), 3);
+});
+Deno.test("solve", () => {
+  assertEquals(solve(`R 5\nU 8\nL 3`), 1);
+});
+
+Deno.test("solve", () => {
+  assertEquals(solve(`R 5\nU 8\nL 8`), 4);
+});
+
+Deno.test("solve", () => {
+  assertEquals(solve(input), 36);
 });
--- ts/day09/part2.ts
+++ ts/day09/part2.ts
@@ -1,5 +1,69 @@
-export const solve = (input: string) => {
-  return input;
+export type Direction = "U" | "D" | "L" | "R";
+export type Coords = [number, number];
+
+export const moveHead = (head: Coords, direction: Direction): Coords => {
+  let [headRow, headCol] = [head[0], head[1]];
+
+  switch (direction) {
+    case "U":
+      headRow += 1;
+      break;
+    case "D":
+      headRow -= 1;
+      break;
+    case "L":
+      headCol -= 1;
+      break;
+    case "R":
+      headCol += 1;
+      break;
+  }
+  return [headRow, headCol];
 };
 
-// console.log(solve(await Deno.readTextFile("day09/input.txt")));
+export const moveTail = (head: Coords, tail: Coords): Coords => {
+  const [headRow, headCol] = [head[0], head[1]];
+  let [tailRow, tailCol] = [tail[0], tail[1]];
+
+  const rowDiff = Math.abs(headRow - tailRow);
+  const colDiff = Math.abs(headCol - tailCol);
+
+  const isOverLapping = headRow === tailRow && headCol === tailCol;
+  const isAdjacent = rowDiff === 1 && colDiff === 1;
+
+  if (isOverLapping || isAdjacent) {
+    return [tailRow, tailCol];
+  }
+
+  if (rowDiff == 2 || colDiff == 2 || (rowDiff > 0 && colDiff > 0)) {
+    if (headRow < tailRow) tailRow--;
+    if (headRow > tailRow) tailRow++;
+    if (headCol < tailCol) tailCol--;
+    if (headCol > tailCol) tailCol++;
+  }
+
+  return [tailRow, tailCol];
+};
+
+export const solve = (input: string) => {
+  let head: Coords = [0, 0];
+  const tails: Coords[] = new Array(9).fill([0, 0]);
+
+  const tailVisited = input.split("\n").reduce((acc, instruction) => {
+    const [direction, distance] = instruction.split(" ");
+    for (let i = 0; i < Number(distance); i++) {
+      head = moveHead(head, direction as Direction);
+      tails[0] = moveTail(head, tails[0]);
+      for (let i = 1; i < 9; i++) {
+        let _;
+        tails[i] = moveTail(tails[i - 1], tails[i]);
+      }
+      acc.add(`${tails[8][0]}-${tails[8][1]}`);
+    }
+    return acc;
+  }, new Set());
+
+  return tailVisited.size;
+};
+
+console.log(solve(await Deno.readTextFile("day09/input.txt")));
